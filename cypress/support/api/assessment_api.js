ea32b5d8d1e64a6605735e589a6a95b4bc7722d0
Sabbir Shanto <sabbirshanto2626@gmail.com>
2025-11-26 15:50:02 +0600

    1st Commit

---
+++ cypress/support/api/assessment_api.js
@@ -0,0 +1,37 @@
+export class AssessmentApi {
+  constructor() {
+    this.baseUrl = 'http://reqres.in/api'
+  }
+  createUser(payload) {
+    return cy.request({
+      method: "POST",
+      url: `${this.baseUrl}/users`,
+      body: payload
+    });
+  }
+
+  getUser(id) {
+    return cy.request({
+      method: "GET",
+      url: `${this.baseUrl}/users/${id}`
+    });
+  }
+
+  updateUser(id, payload) {
+    return cy.request({
+      method: "PUT",
+      url: `${this.baseUrl}/users/${id}`,
+      body: payload
+    });
+  }
+
+  deleteUser(id) {
+    return cy.request({
+      method: "DELETE",
+      url: `${this.baseUrl}/users/${id}`
+    });
+  }
+
+}
+
+export const assessmentApi = new AssessmentApi()
